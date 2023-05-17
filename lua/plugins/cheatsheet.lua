b9a221d03c2fe269ad81521b04da5caa3f7d7a44
Lol McBride <3874987+lolmc@users.noreply.github.com>
2023-05-17 22:34:04 +0100

    Updates to LazyNvim config - plugins

---
+++ lua/plugins/cheatsheet.lua
@@ -0,0 +1,7 @@
+return {
+  -- cheatsheet plugin
+  {
+    "sudormrfbin/cheatsheet.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/popup.nvim", "nvim-lua/plenary.nvim" },
+  },
+}
---
+++ lua/plugins/vimbegood.lua
@@ -0,0 +1,4 @@
+return {
+  -- add the vim-be-good plugin
+  { "ThePrimeagen/vim-be-good", dependencies = { "nvim-lua/plenary.nvim" } },
+}
